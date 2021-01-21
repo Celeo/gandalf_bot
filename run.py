f2c6cbd58d4aa8659b3df7237c2c7b27cee5f58f
Celeo <mattboulanger@fastmail.com>
2021-01-20 21:17:49 -0800

    Dockerize

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
+import os
+import shutil
+import sys
+
+
+if not os.path.exists("config.json"):
+    if not os.path.exists("/data/config.json"):
+        print("No 'config.json' in current data or '/data'", file=sys.stderr)
+        sys.exit(1)
+    shutil.copy("/data/config.json", "config.json")
+    print("Using 'config.json' from '/data'")
+
+
 from gandalf_bot import main
 
 
