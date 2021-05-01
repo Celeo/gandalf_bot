2a0a1c56d30b969da25d29c127b641f56f858205
Celeo <mattboulanger@fastmail.com>
2021-05-01 14:35:46 -0700

    Finish role reactions

--- lib/roles.ex
+++ lib/roles.ex
@@ -1,16 +1,15 @@
 defmodule Bot.Roles do
   require Logger
 
-  def reaction_add(data) do
+  defp handle_reaction!(guild_id, channel_id, message_id, user_id, username, emoji_name, add) do
     db_roles = Bot.Config.DB.get!()
-    guild_id = data[:guild_id]
     guild_roles = Nostrum.Api.get_guild_roles!(guild_id)
 
     matches_from_db =
       Enum.filter(db_roles, fn row ->
-        Enum.at(row, 0) == data[:channel_id] &&
-          Enum.at(row, 1) == data[:message_id] &&
-          Enum.at(row, 2) == data[:emoji][:name]
+        Enum.at(row, 0) == channel_id &&
+          Enum.at(row, 1) == message_id &&
+          Enum.at(row, 2) == emoji_name
       end)
 
     if length(matches_from_db) == 0 do
@@ -28,25 +27,52 @@ defmodule Bot.Roles do
           )
 
         guild_role ->
-          Logger.debug("Adding role #{db_entry_name} to #{data[:member][:user][:username]}")
+          if add do
+            Logger.debug("Adding role #{db_entry_name} to #{username}")
 
-          {:ok} =
-            Nostrum.Api.add_guild_member_role(
-              guild_id,
-              data[:member][:user][:id],
-              guild_role.id
-            )
+            {:ok} =
+              Nostrum.Api.add_guild_member_role(
+                guild_id,
+                user_id,
+                guild_role.id
+              )
+          else
+            Logger.debug("Removing role #{db_entry_name} to #{username}")
+
+            {:ok} =
+              Nostrum.Api.remove_guild_member_role(
+                guild_id,
+                user_id,
+                guild_role.id
+              )
+          end
       end
     end)
   end
 
+  def reaction_add(data) do
+    handle_reaction!(
+      data[:guild_id],
+      data[:channel_id],
+      data[:message_id],
+      data[:member][:user][:id],
+      data[:member][:user][:username],
+      data[:emoji][:name],
+      true
+    )
+  end
+
   def reaction_remove(data) do
-    Logger.debug("reaction_remove: #{inspect(data)}")
+    user_obj = Nostrum.Api.get_user!(data[:user_id])
 
-    db_roles = Bot.Config.DB.get!()
-    guild_id = data[:guild_id]
-    guild_roles = Nostrum.Api.get_guild_roles!(guild_id)
-
-    # ...
+    handle_reaction!(
+      data[:guild_id],
+      data[:channel_id],
+      data[:message_id],
+      data[:user_id],
+      user_obj.username,
+      data[:emoji][:name],
+      false
+    )
   end
 end
