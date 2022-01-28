1c1964f14658f5c1d04cace6339d5c95ef180b76
Celeo <mattboulanger@fastmail.com>
2022-01-27 19:12:13 -0800

    Refactor

--- src/configWorker.ts
+++ src/configWorker.ts
@@ -3,14 +3,10 @@
 
 import { loadConfig } from "./config.ts";
 
-async function loadAndPost(): Promise<void> {
-  (self as unknown as Worker).postMessage(await loadConfig());
-}
-
 const watcher = Deno.watchFs("./config.json");
 
 for await (const event of watcher) {
   if (event.kind === "modify") {
-    await loadAndPost();
+    (self as unknown as Worker).postMessage(await loadConfig());
   }
 }
