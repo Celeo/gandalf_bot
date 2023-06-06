70647e6243b66ee8d83aafd67306c4fa4113358d
Celeo <mattboulanger@fastmail.com>
2023-06-05 22:11:08 -0700

    Add back background tasks

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@
 use crate::config::{load as load_config, Config};
 use anyhow::Result;
 use base64::{engine::general_purpose, Engine as _};
+use chrono::{Datelike, TimeZone};
 use dotenv::dotenv;
-use log::{error, info, warn};
-use std::{env, sync::Arc};
+use log::{debug, error, info, warn};
+use std::{collections::HashMap, env, sync::Arc, time::Duration};
+use tokio::time::sleep;
 use twilight_gateway::{Event, Intents, Shard, ShardId};
 use twilight_http::Client as HttpClient;
 use twilight_interactions::command::CreateCommand;
@@ -32,6 +34,73 @@ fn bot_id_from_token(token: &str) -> u64 {
     .unwrap()
     .parse()
     .unwrap()
+}
+
+/// Loop, sending birthday messages to the configured channel
+/// every 6 hours on matching days.
+async fn birthday_loop(
+    config: Arc<Config>,
+    http: Arc<HttpClient>,
+    posted: &mut HashMap<u64, Vec<i32>>,
+) -> Result<()> {
+    debug!("Checking for birthdays");
+    let now = chrono_tz::US::Pacific.from_utc_datetime(&chrono::offset::Utc::now().naive_utc());
+    let now_str = format!("{}/{}", now.month(), now.day());
+    for entry in &config.birthdays {
+        if entry.when == now_str {
+            if posted
+                .get(&entry.who)
+                .unwrap_or(&Vec::new())
+                .contains(&now.year())
+            {
+                continue;
+            }
+            info!("Sending birthday message");
+            http.create_message(Id::new(config.birthday_channel))
+                .content(&format!("Happy birthday to <@!${}>!", entry.who))?
+                .await?;
+            posted
+                .entry(entry.who)
+                .and_modify(|d| d.push(now.year()))
+                .or_insert(vec![now.year()]);
+        }
+    }
+    Ok(())
+}
+
+/// Loop, sending book reminder alerts every 12 hours on matching days.
+async fn book_loop(config: Arc<Config>, http: Arc<HttpClient>, posted: &mut Vec<u8>) -> Result<()> {
+    debug!("Checking for book reminders");
+    if config.book_reminders.is_empty() {
+        return Ok(());
+    }
+    let now = chrono_tz::US::Pacific.from_utc_datetime(&chrono::offset::Utc::now().naive_utc());
+    #[allow(clippy::cast_possible_truncation)]
+    let day = now.day() as u8; // no idea why they don't have the day as u8 anyway
+
+    if config.book_reminders.contains(&day) && !posted.contains(&day) {
+        let message = match config.book_reminders.iter().position(|&b| b == day) {
+            Some(0) => "First book reminder! 25% of the way through the month.",
+            Some(1) => "Second book reminder! Halfway through the month.",
+            Some(2) => "Third book reminder! 75% through the month!",
+            Some(3) => "Last book reminder! Finish by tomorrow!",
+            Some(_) => {
+                error!("OOB day index");
+                return Ok(());
+            }
+            None => {
+                error!("Could not get day index");
+                return Ok(());
+            }
+        };
+        info!("Sending book reminder message");
+        http.create_message(Id::new(config.book_channel))
+            .content(message)?
+            .await?;
+        posted.push(day);
+    }
+
+    Ok(())
 }
 
 /// Entrypoint.
@@ -73,6 +142,42 @@ async fn main() {
         }
     };
 
+    {
+        debug!("Starting birthday task");
+        let http = Arc::clone(&http);
+        let config = Arc::clone(&config);
+        tokio::spawn(async move {
+            let mut posted = HashMap::new();
+            loop {
+                sleep(Duration::from_millis(1_000 * 30)).await; // 30s
+                let http = Arc::clone(&http);
+                let config = Arc::clone(&config);
+                if let Err(e) = birthday_loop(config, http, &mut posted).await {
+                    error!("Issue in birthday task: {e}");
+                }
+                sleep(Duration::from_millis(1_000 * 60 * 60 * 6)).await; // 6hrs
+            }
+        });
+    }
+
+    {
+        debug!("Starting book task");
+        let http = Arc::clone(&http);
+        let config = Arc::clone(&config);
+        tokio::spawn(async move {
+            let mut posted = Vec::new();
+            loop {
+                sleep(Duration::from_millis(1_000 * 30)).await; // 30s
+                let http = Arc::clone(&http);
+                let config = Arc::clone(&config);
+                if let Err(e) = book_loop(config, http, &mut posted).await {
+                    error!("Issue in book task: {e}");
+                }
+                sleep(Duration::from_millis(1_000 * 60 * 60 * 12)).await; // 12hrs
+            }
+        });
+    }
+
     info!("Waiting for events");
     loop {
         let event = match shard.next_event().await {
@@ -86,7 +191,7 @@ async fn main() {
             }
         };
 
-        let http: Arc<HttpClient> = Arc::clone(&http);
+        let http = Arc::clone(&http);
         let config = Arc::clone(&config);
         tokio::spawn(async move {
             if let Err(e) = handle_event(event, config, http, bot_id).await {
