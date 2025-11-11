3a115eba9335d8089caa0d1bef4f20fb6aa66cd8
Celeo <mattboulanger@fastmail.com>
2025-11-11 11:20:31 -0800

    Change member has role before removing

--- src/commands.rs
+++ src/commands.rs
@@ -300,13 +300,16 @@ pub async fn handler(
                 }
                 "color_none" => {
                     let roles = http.roles(event.guild_id.unwrap()).await?.model().await?;
+                    let member = event.member.as_ref().unwrap().clone();
                     for role in &roles {
-                        http.remove_guild_member_role(
-                            event.guild_id.unwrap(),
-                            event.member.as_ref().unwrap().user.as_ref().unwrap().id,
-                            role.id,
-                        )
-                        .await?;
+                        if member.roles.contains(&role.id) {
+                            http.remove_guild_member_role(
+                                event.guild_id.unwrap(),
+                                event.member.as_ref().unwrap().user.as_ref().unwrap().id,
+                                role.id,
+                            )
+                            .await?;
+                        }
                     }
                     resp(event, &interaction, "👍").await?;
                 }
