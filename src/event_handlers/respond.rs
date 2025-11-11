dfe8120f071ae12ee724391b86059ae47fb05a4c
Celeo <mattboulanger@fastmail.com>
2025-11-11 14:04:18 -0800

    Rare chance for extra quote

--- src/event_handlers/respond.rs
+++ src/event_handlers/respond.rs
@@ -1,6 +1,6 @@
 use crate::quotes::MESSAGES;
 use anyhow::Result;
-use rand::seq::SliceRandom;
+use rand::{seq::SliceRandom, Rng};
 use std::sync::Arc;
 use twilight_gateway::Event;
 use twilight_http::Client;
@@ -13,7 +13,11 @@ pub async fn handler(e: &Event, http: &Arc<Client>, bot_id: u64) -> Result<()> {
             .iter()
             .any(|mention| mention.id == Id::new(bot_id));
         if mentioned_self && !event.mention_everyone {
-            let response = MESSAGES.choose(&mut rand::thread_rng()).unwrap();
+            let response = if rand::thread_rng().gen_range(0..500) == 1 {
+                "All right then, keep your fucking secrets."
+            } else {
+                MESSAGES.choose(&mut rand::thread_rng()).unwrap()
+            };
             http.create_message(event.channel_id)
                 .reply(event.id)
                 .content(response)?
